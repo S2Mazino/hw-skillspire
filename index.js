3df66dbe99bc89cf7f9022cdd9cddca994610ca4
Nordine Chaumath <nordine.chaumath@hotmail.com>
2018-11-30 17:03:19 -0800

    added background image using css

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function mathFunction(a, b) {
     return a * b
 }
 mathFunction(3, 4)
+
+var dungeon = {
+    "cdev": "yes",
+    "cmoc": "yes",
+    "cpap": "no"
+};
+
+var dun = [];
+dun.cdev = "yes";
+dun.cmoc = "no";
+console.log(dun);
