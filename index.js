d4d241799776d0dd8e8c7df15a6b1500cc993a29
Nordine Chaumath <nordine.chaumath@hotmail.com>
2018-12-02 19:58:27 -0800

    made a function for userAge

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function mathFunction(a, b) {
     return a + 1 * b
 }
 mathFunction(3, 4)
+
+
+function person(){
+    if(userAge < 67){
+        console.log("This person is senior")
+    } else if(userAge < 18){
+        console.log("This person is still a child")
+    } else console.log ("This person is an adult")
+}
