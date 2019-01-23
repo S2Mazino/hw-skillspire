7a7439b144e1ebd26ab78cebdb2e533134dcf265
S2Mazino <44010904+S2Mazino@users.noreply.github.com>
2019-01-22 22:57:07 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,6 @@ function mathFunction(a, b) {
 }
 mathFunction(3, 4)
 
-<<<<<<< HEAD
-//function that gives the maximum length difference.
-//Codewar assignment
-function mxdiflg(a1, a2) {
-
-    if(a1.length===0 || a2.length===0){
-    return -1
-    }
-    
-    var biggestDifference = 0;
-    
-    for(var i=0; i<a1.length;i++){
-      for(var j=0;j<a2.length;j++){
-        if(Math.abs(a1[i].length - a2[j].length) > biggestDifference){
-          biggestDifference = Math.abs(a1[i].length - a2[j].length)
-    }
-    }
-    }return biggestDifference
-    }
-=======
-
 function person(){
     if(userAge < 67){
         console.log("This person is senior")
@@ -32,4 +11,3 @@ function person(){
         console.log("This person is still a child")
     } else console.log ("This person is an adult")
 }
->>>>>>> fc4254d5c0ba47c3ce92abbd3f64a0f8ddc211fe
